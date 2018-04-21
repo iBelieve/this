05edf0a30872bcd69b4ba05c13968e5d0ce8dac4
Michael Spencer <sonrisesoftware@gmail.com>
2018-04-21 10:55:28 -0500

    Add test for Meson project

---
+++ tests/test_meson.py
@@ -0,0 +1,25 @@
+import pytest
+
+from thiscli.project import Project
+from thiscli.project.meson import MesonProject
+from . import setup_dir, runs_commands
+
+
+@pytest.fixture
+def meson_dir(mocker):
+    setup_dir(mocker, 'meson.build')
+
+
+@pytest.fixture
+def meson_project(meson_dir):
+    return Project.find()
+
+
+def test_find_meson_project(meson_project):
+    assert isinstance(meson_project, MesonProject)
+
+
+def test_build_meson_project(meson_project):
+    with runs_commands('meson setup build --prefix=$HOME/.local',
+                       'ninja -C build'):
+        meson_project.build()
