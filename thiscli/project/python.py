ee30f3dae233066c0597fd49b7ca973e38219e33
Michael Spencer <sonrisesoftware@gmail.com>
2018-04-23 12:30:59 -0500

    python: Add support for pytest

--- thiscli/project/python.py
+++ thiscli/project/python.py
@@ -1,29 +1,54 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 import os
 
 from . import Project, delayed_exit
 from ..util import fatal
 
 
-class PythonEnv:
-    pass
+class PythonEnv(ABC):
+    @abstractmethod
+    def has_package(self, name):
+        pass
 
 
 class PythonPipenv(PythonEnv):
     description = 'Pipenv'
 
+    def __init__(self, project):
+        with open(project.path('Pipfile')) as f:
+            self.pipfile = f.read()
+
+    def has_package(self, name):
+        return (name + ' = ') in self.pipfile
+
 
 class PythonPipTools(PythonEnv):
-    description = 'requirements.txt'
+    description = 'pip-tools'
+
+    def __init__(self, project):
+        with open(project.path('requirements.in')) as f:
+            self.requirements = f.read()
+
+    def has_package(self, name):
+        return name in self.requirements
 
 
 class PythonRequirements(PythonEnv):
     description = 'requirements.txt'
 
+    def __init__(self, project):
+        with open(project.path('requirements.txt')) as f:
+            self.requirements = f.read()
 
-class PythonProject(Project, ABC):
+    def has_package(self, name):
+        return name in self.requirements
+
+
+class PythonProject(Project):
     def __init__(self, cwd):
         super().__init__(cwd)
+
+        self.has_setup = self.exists('setup.py')
 
         self.packages = [dirname for dirname in os.listdir(cwd)
                          if self.exists(dirname, '__init__.py')]
@@ -31,15 +56,16 @@ class PythonProject(Project, ABC):
             fatal("No python packages containing __init__.py found")
 
         if self.exists('Pipfile'):
-            self.env = PythonPipenv()
+            self.env = PythonPipenv(self)
         elif self.exists('requirements.in'):
-            self.env = PythonPipTools()
+            self.env = PythonPipTools(self)
         elif self.exists('requirements.txt'):
-            self.env = PythonRequirements()
+            self.env = PythonRequirements(self)
         else:
             self.env = None
 
-        self.can_build = self.can_test = self.exists('setup.py')
+        self.can_build = self.has_setup
+        self.can_test = self.has_setup or self.has_package('pytest')
 
     @classmethod
     def find(cls):
@@ -54,22 +80,30 @@ class PythonProject(Project, ABC):
         if self.env:
             description += ' using ' + self.env.description
 
-        if self.exists('setup.py'):
+        if self.has_setup:
             if 'using' in description:
                 description += ' and setup.py'
             else:
                 description += ' using setup.py'
         return description
 
+    def has_package(self, name):
+        if self.env:
+            return self.env.has_package(name)
+        else:
+            return False
+
     def build(self):
-        if self.exists('setup.py'):
+        if self.has_setup:
             self.cmd('python setup.py build')
         else:
             super().build()
 
     def test(self):
-        if self.exists('setup.py'):
+        if self.has_setup:
             self.cmd('python setup.py test')
+        elif self.has_package('pytest'):
+            self.cmd('pytest')
         else:
             super().test()
 
