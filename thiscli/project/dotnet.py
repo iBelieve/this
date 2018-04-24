d0a7705763eeb78fa055ad1c75e79fe7cf4c5cf9
Tim Süberkrüb <dev@timsueberkrueb.io>
2018-04-24 22:09:55 +0200

    Add initial support for .NET Core

---
+++ thiscli/project/dotnet.py
@@ -0,0 +1,25 @@
+from . import Project
+from ..env import is_env_release
+
+
+class DotnetCoreProject(Project):
+    description = '.NET Core project'
+
+    @classmethod
+    def find(cls):
+        return cls.find_containing('*.csproj')
+
+    def build(self, env):
+        if is_env_release(env):
+            self.cmd("dotnet build -c release")
+        else:
+            self.cmd("dotnet build")
+
+    def test(self):
+        self.cmd("dotnet test")
+
+    def run(self, env):
+        if is_env_release(env):
+            self.cmd("dotnet run -c release")
+        else:
+            self.cmd("dotnet run")
