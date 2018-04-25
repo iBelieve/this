3b1b464c93fe23c87887f094b867513b29c65334
Michael Spencer <sonrisesoftware@gmail.com>
2018-04-24 20:19:18 -0500

    Exclude tests package in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author_email='sonrisesoftware@gmail.com',
     url='https://github.com/ibelieve/this',
     license='MIT',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=[
         'Click',
